51cdffab26e88340ec02cdf56b8f7a8e97dae21d
Ezeki Zibzibadze <ez@jok.io>
2021-12-31 13:18:10 +0400

    feat: export ObjectId from mod.js

--- mod.ts
+++ mod.ts
@@ -1,2 +1,5 @@
+import {Bson} from './deps.ts'
+
 export { MangoRepo, type MangoRepoOptions } from './src/mangoRepo.ts'
 export { connectMongo } from './src/connectMongo.ts'
+export const ObjectId = Bson.ObjectId
