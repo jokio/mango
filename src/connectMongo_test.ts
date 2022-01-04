5e5aa08a76e7207d62b765490dcdfe2b3daf5174
Ezeki Zibzibadze <ez@jok.io>
2022-01-04 15:09:56 +0400

    test: imrpove coverage

--- src/connectMongo_test.ts
+++ src/connectMongo_test.ts
@@ -10,9 +10,27 @@ Deno.test('should fire error on empty connectionString', () => {
   )
 })
 
-Deno.test('should connect successfully', () =>
-  withDb((db, client) => {
-    assertExists(db)
-    assertExists(client)
-  }),
-)
+Deno.test('should fire error on null connectionString', () => {
+  assertRejects(
+    // deno-lint-ignore no-explicit-any
+    () => connectMongo(null as any),
+    Error,
+    'Invalid connection string',
+  )
+
+  assertRejects(
+    // deno-lint-ignore no-explicit-any
+    () => connectMongo(undefined as any),
+    Error,
+    'Invalid connection string',
+  )
+})
+
+Deno.test('should connects successfully', async () => {
+  const result = await connectMongo('mongo://127.0.0.1/test')
+  assertExists(result)
+  assertExists(result.client)
+  assertExists(result.db)
+
+  await result.client.close()
+})
