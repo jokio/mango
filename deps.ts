75b1dc28e8acbfdd6709b4955cd649dad0126935
Ezeki Zibzibadze <ez@jok.io>
2023-02-12 17:55:50 +0400

    feat: update to latest mongo driver

--- deps.ts
+++ deps.ts
@@ -6,6 +6,8 @@ export {
   type Filter,
   type FindAndModifyOptions,
   type FindOptions,
+  type InsertDocument,
+  type ObjectId,
   type UpdateFilter,
   type UpdateOptions,
-} from 'https://deno.land/x/mongo@v0.29.0/mod.ts'
+} from 'https://deno.land/x/mongo@v0.31.1/mod.ts'
